4de07028824a003b95bd63d8f158c75eccb35ce6
Yami Medina <yamilethmedina1@gmail.com>
2017-10-21 14:28:18 -0400

    successful migration of permits

--- db/migrate/20171021182311_create_permits.rb
+++ db/migrate/20171021182311_create_permits.rb
@@ -4,7 +4,7 @@ class CreatePermits < ActiveRecord::Migration[5.1]
       t.float :lat
       t.float :long
       t.datetime :status_date
-      t.int :city_source_id
+      t.integer :city_source_id
       t.string :address
 
       t.timestamps
