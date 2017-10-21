83a333c7f8a5875fb2227511de0fff3897ee6f22
Bryce Kerley <bkerley@brycekerley.net>
2017-10-21 15:35:05 -0400

    implement fixup

--- app/models/city_source.rb
+++ app/models/city_source.rb
@@ -12,4 +12,30 @@ class CitySource < ApplicationRecord
       cs.save!
     end
   end
+
+  def self.fixup!
+    Permit.delete_all
+    File.open(Rails.root.join('log', 'fixup_permits.log'), 'w') do |l|
+      self.in_batches.each_record do |cs|
+        p = Permit.new
+        begin
+          addr = MiamiDadeGeo::Address.new_from_address cs.deliveryaddress
+          p.lat = addr.lat
+          p.long = addr.long
+        rescue => e
+          l.puts e
+          l.puts cs.deliveryaddress
+        end
+        p.status_date = Time.parse cs.statusdate
+        p.city_source_id = cs.id
+        p.address = cs.deliveryaddress
+        p.save!
+        if p.lat
+          print '.'
+        else
+          print 'x'
+        end
+      end
+    end
+  end
 end
