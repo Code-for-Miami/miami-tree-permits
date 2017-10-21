3de62df94aac581f7f41fe7d94a403c41fddd032
Bryce Kerley <bkerley@brycekerley.net>
2017-10-21 14:26:05 -0400

    city_source import

--- app/models/city_source.rb
+++ app/models/city_source.rb
@@ -1,2 +1,15 @@
 class CitySource < ApplicationRecord
+
+  def self.import!
+    permits = SODA_CLIENT.get('kf7n-cdpf',
+                        {scopeofwork: 'TREE PERMIT',
+                         '$order' => 'statusdate ASC'})
+
+    permits.each do |permit|
+      cs = self.new
+      cs.attributes = permit.to_hash
+      cs.location_1 = permit.location_1.join ', '
+      cs.save!
+    end
+  end
 end
